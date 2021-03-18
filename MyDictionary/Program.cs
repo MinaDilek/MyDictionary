5c097ac835d38cdbf20cd37c5c366f7421eaf004
mina dilek <minadilek7@outlook.com>
2021-03-18 21:32:01 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> AdYas = new Dictionary<string, int>();
+            AdYas.Add("mina", 24);
+            AdYas.Add("hilal", 23);
+            AdYas.Add("nisa", 15);
+
+            foreach (var degeroku in AdYas)
+            {
+                Console.WriteLine(degeroku);
+            }
+
+            var eleman = AdYas.Count();
+            Console.WriteLine();
+            Console.WriteLine("eleman sayisi: " + eleman);
+        }
+    }
+}
